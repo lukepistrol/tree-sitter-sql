fb79fdfb7413906a6ca2e0833c57d1cb38cc00f2
Lukas Pistrol <lukas@pistrol.com>
2023-02-25 13:17:09 +0100

    add parser.c to Package.swift

--- Package.swift
+++ Package.swift
@@ -22,6 +22,7 @@ let package = Package(
                     "README.md",
                 ],
                 sources: [
+                    "src/parser.c",
                     "src/scanner.cc",
                 ],
                 resources: [
